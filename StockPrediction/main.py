061d889655f7aafb195ba435a5458a53c8a6718b
Terence Dumartin <dumartin.terence@gmail.com>
2021-06-01 13:17:36 +0200

    add Candlestick Chart

--- StockPrediction/main.py
+++ StockPrediction/main.py
@@ -6,6 +6,7 @@ import yfinance as yf
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
+import plotly.graph_objects as goj
 
 START = "2015-01-01"
 TODAY = date.today().strftime("%Y-%m-%d")
@@ -13,10 +14,6 @@ TODAY = date.today().strftime("%Y-%m-%d")
 st.title("Stock Prediction")
 
 stocks = st.text_input('Ticker symbol', 'AAPL')
-#selected_stock = st.selectbox("Select dataset for prediction", stocks)
-
-n_years = st.slider("Years of prediction:", 1, 4)
-period = n_years * 365
 
 @st.cache(allow_output_mutation=True)
 def load_data(ticker):
@@ -30,34 +27,55 @@ data_load_state.text("Loading data... done!")
 data['Date'] = pd.to_datetime(data['Date']).dt.date
 
 st.subheader('Raw data')
-st.write('The only last 5 days is displayed')
-st.write(data.tail())
+st.write('*Data Available for the models*', data.shape[0] * data.shape[1])
+st.write(data[::-1])
 
-def plot_raw_data():
-    fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
-    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
-    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
-    st.plotly_chart(fig)
+def plot_chart_raw_data():
+    fig3 = go.Figure(data=[goj.Candlestick(x=data['Date'],
+        open=data['Open'],
+        high=data['High'],
+        low=data['Low'],
+        close=data['Close'])])
+    fig3.update_xaxes(type="category")
+    fig3.update_layout(title_text="Candlestick Chart", height=600)
+    st.plotly_chart(fig3, use_container_width=True)
 
-plot_raw_data()
+plot_chart_raw_data()
 
-#Forecasting
-df_train = data[['Date', 'Close']]
-df_train = df_train.rename(columns={'Date': 'ds', 'Close': 'y'})
+# def plot_raw_data():
+#     fig = go.Figure()
+#     fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name='stock_open'))
+#     fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name='stock_close'))
+#     fig.layout.update(title_text="Chart", xaxis_rangeslider_visible=True)
+#     st.plotly_chart(fig)
 
-m = Prophet()
-m.fit(df_train)
-future = m.make_future_dataframe(periods=period)
-forecast = m.predict(future)
+# plot_raw_data()
 
-st.subheader('Forecast Data')
-fig1 = plot_plotly(m, forecast)
-st.plotly_chart(fig1)
+st.write('''
+    <style>
+        div.row-widget.stRadio > div{flex-direction:row;}
+        .row-widget.stButton{text-align:center}
+    </style>''', unsafe_allow_html=True)
+n_years = st.radio('Years of predictions', (1, 2, 3, 4))
+period = n_years * 365
 
-st.subheader('Forecast Intervales')
-fig2 = m.plot_components(forecast)
-st.write(fig2)
+if st.button(f"Predict the next {n_years} years"):
+    #Forecasting
+    df_train = data[['Date', 'Close']]
+    df_train = df_train.rename(columns={'Date': 'ds', 'Close': 'y'})
+
+    m = Prophet()
+    m.fit(df_train)
+    future = m.make_future_dataframe(periods=period)
+    forecast = m.predict(future)
+
+    st.subheader('Forecast Data')
+    fig1 = plot_plotly(m, forecast)
+    st.plotly_chart(fig1)
+
+    st.subheader('Forecast Intervales')
+    fig2 = m.plot_components(forecast)
+    st.write(fig2)
 
 
 
